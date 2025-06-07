16a7a6e7a2a13fbc832b0d18ddda7c5e00af1b84
HDzungx <hdzungx@gmail.com>
2025-06-07 10:40:59 +0700

    Initialized Dolby Atmos
    Co-Authored-By: ahnet-69 <syedmusicrealise@gmail.com>
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: HDzungx <hdzungx@gmail.com>

---
+++ dolby-vendor.mk
@@ -0,0 +1,28 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/dolby
+
+PRODUCT_COPY_FILES += \
+    hardware/dolby/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
+    hardware/dolby/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc
+
+PRODUCT_PACKAGES += \
+    libdapparamstorage \
+    libdlbpreg \
+    libdlbvol \
+    libswdap \
+    libswgamedap \
+    libswvqe \
+    vendor.dolby.hardware.dms@2.0 \
+    libcodec2_soft_ac4dec \
+    libcodec2_soft_ddpdec \
+    libcodec2_soft_dolby \
+    libcodec2_store_dolby \
+    libdeccfg \
+    libdlbdsservice \
+    vendor.dolby.hardware.dms@2.0-impl \
+    vendor.dolby.hardware.dms@2.0-service \
+    vendor.dolby.media.c2@1.0-service
